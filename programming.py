5b5ea06beca1644da8174a07a5124accfe7229de
anamika-dho03 <dhondiyalanamika@gmail.com>
2025-05-09 15:41:52 +0530

    Update programming.py

--- programming.py
+++ programming.py
@@ -1,16 +1,1 @@
-import list1
-import set2
-import dict3
 
-# Example Usage:
-lst = [1, 2, 3]
-list1.append1(lst, 4)
-print(lst)  # [1, 2, 3, 4]
-
-s = {1, 2, 3}
-set2.adds2(s, 4)
-print(s)  # {1, 2, 3, 4}
-
-d = {"a": 1, "b": 2}
-dict3.add3(d, "c", 3)
-print(d)  # {'a': 1, 'b': 2, 'c': 3}
